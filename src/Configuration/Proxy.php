d52bd7cd938eb3221b7860a8f207be972361aa7f
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2015-06-22 22:19:54 +0200

    Added missing PHPDoc

--- src/Configuration/Proxy.php
+++ src/Configuration/Proxy.php
@@ -13,23 +13,40 @@ namespace phpbu\Laravel\Configuration;
 class Proxy
 {
     /**
+     * Configuration
+     *
      * @var array
      */
     private $config;
 
+    /**
+     * Constructor
+     *
+     * @param array $config
+     */
     public function __construct(array $config)
     {
         $this->config = $config;
     }
 
+    /**
+     * Return config value.
+     * Use foo.bar to get $config['foo']['bar']
+     *
+     * @param  string $key
+     * @throws \phpbu\Laravel\Configuration\Exception
+     * @return array
+     */
     public function get($key = null)
     {
         $conf = $this->config;
         if (null !== $key) {
+            $path = '';
             $keys = explode('.', $key);
             foreach ($keys as $k) {
+                $path .= $k;
                 if (!array_key_exists($k, $conf)) {
-                    throw new Exception('invalid configuration key: ' . $k);
+                    throw new Exception('invalid configuration key ' . $key . ' at ' . $path);
                 }
                 $conf = $conf[$k];
             }
